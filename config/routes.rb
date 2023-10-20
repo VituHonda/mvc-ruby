53e86d8e4bb4b619fc4662fa15cce29063595a43
vituhonda <vitorutk@gmail.com>
2023-10-20 15:27:03 -0300

    refactor(usersroute): changed users index route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :produtos
   resources :users
-  get 'test', to: 'users#index'
+  get 'usuarios', to: 'users#index'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
